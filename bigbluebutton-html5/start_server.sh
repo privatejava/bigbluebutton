edd91d6b0b31c2c6d2087fedef9fcb5d306e681a
Ubuntu <ubuntu@ip-10-0-0-36.ap-southeast-1.compute.internal>
2018-03-18 05:27:13 +0000

    updated script

--- bigbluebutton-html5/start_server.sh
+++ bigbluebutton-html5/start_server.sh
@@ -5,7 +5,7 @@ export METEOR_SETTINGS=$(cat programs/server/assets/app/config/settings-producti
 # clear any old collections from the database
 mongo html5client --eval "printjson(db.dropDatabase())"
 
-cd /usr/share/meteor/bundle
+#cd /usr/share/meteor/bundle
 ROOT_URL=http://127.0.0.1/html5client MONGO_URL=mongodb://localhost:27017/html5client NODE_ENV=$ENVIRONMENT_TYPE PORT=3000 node main.js
 
 
